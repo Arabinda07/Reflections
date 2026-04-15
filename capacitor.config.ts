b94fa6aca112a43823e625b451514576a583be67
Arabinda07 <robinsaha434@gmail.com>
2026-04-15 09:41:20 +0530

    adding capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arabinda.reflections',
+  appName: 'Reflections',
+  webDir: 'dist'
+};
+
+export default config;
