fd4a9c52ffde1c8b0777930f40b7837e90cc57c5
Arabinda07 <robinsaha434@gmail.com>
2026-04-30 23:44:37 +0530

    feat: initialize Android application project with Capacitor configuration, plugin registration, and build pipeline

--- android/app/src/main/java/com/arabinda/reflections/MainActivity.java
+++ android/app/src/main/java/com/arabinda/reflections/MainActivity.java
@@ -1,14 +1,8 @@
 package com.arabinda.reflections;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.CapacitorPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
